1157276c92834a0c4032a42d999c0d8e095a34bd
Burnsedia <bburnsed1995@gmail.com>
2023-09-14 15:29:26 -0400

    Updated Game State

--- Globals/Events.gd
+++ Globals/Events.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal GameOVER
 signal BOSSMODE
+signal WAVEMODE
--- Globals/Glabals.gd
+++ Globals/Glabals.gd
@@ -1,12 +1,37 @@
 extends Node
 
-enum {
+
+func _ready():
+	Events.connect("GameOVER", self, "setGameOver")
+	Events.connect("WAVEMODE", self, "setWaveMode")
+	Events.connect("BOSSMODE",self, "setBossMode")
+	
+enum States {
 	BOSSMODE,
 	GAMEOVER, 
 	WAVEMODE
 }
-
+var state = States.WAVEMODE
 var score = 0
 
 func GameState():
-	pass
+	match state:
+		States.GAMEOVER:
+			setGameOver()
+		States.WAVEMODE:
+			setWaveMode()
+		States.BOSSMODE:
+			setBossMode()
+	
+	
+	
+func setGameOver():
+	state = States.GAMEOVER
+	get_tree().reload_current_scene()
+	score = 0
+	
+func setWaveMode():
+	state = States.WAVEMODE
+	
+func setBossMode():
+	state = States.BOSSMODE
