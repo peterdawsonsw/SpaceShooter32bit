3672017648810cf94ba521b4f87c31aa8e31e171
Burnsedia <bburnsed1995@gmail.com>
2023-09-19 19:10:34 -0400

    cleaned up coded, remvoed unneed functions

--- NPC/SWARM/Impact.gd
+++ NPC/SWARM/Impact.gd
@@ -1,17 +1,6 @@
 extends Area
 
-
 var damage = 5
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Area_body_entered(body):
 	body.takeDamage(damage)
