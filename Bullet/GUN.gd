c98412f9b0d272fefedc26998aaf73ace4e2e37f
Burnsedia <bburnsed1995@gmail.com>
2023-09-21 19:14:24 -0400

    Added SoundEffect Logic

--- Bullet/GUN.gd
+++ Bullet/GUN.gd
@@ -3,6 +3,7 @@ extends Spatial
 
 var BULLET =  preload("res://Bullet/Bullet.tscn")
 var main
+onready var sournd = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	main = get_tree().current_scene
@@ -16,3 +17,5 @@ func fire():
 	var bullet = BULLET.instance()
 	main.add_child(bullet)
 	bullet.transform = global_transform
+	sournd.play(0.0)
+	
